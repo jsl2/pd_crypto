c4438ea414321c4e6c5aaa6c08ede90a13fcd597
Jonathan Lowe <jonathan.lowe17@gmail.com>
2019-03-13 10:29:03 +0100

    Add files via upload

--- util.h
+++ util.h
@@ -1,10 +1,12 @@
 #ifndef _UTIL_H_INCL
 #define _UTIL_H_INCL
 
-#define SUCCESS 0
-#define FAILURE 1
+#define SUCCESS 1
+#define FAILURE 0
 
 void print_radix(uint16_t* x);
+
+uint32_t zero_pad(uint16_t* x, uint16_t zeros);
 
 uint32_t mp_add(uint16_t* x, uint16_t* y, uint16_t* w);
 
@@ -12,6 +14,14 @@ uint32_t mp_sub(uint16_t* x, uint16_t* y, uint16_t* w);
 
 uint32_t mp_mult(uint16_t* x, uint16_t* y, uint16_t* w);
 
+uint32_t mp_mult_scalar(uint16_t* x, uint16_t y, uint16_t* w);
+
+uint32_t mp_div(uint16_t* x, uint16_t* y, uint16_t* q, uint16_t* r);
+
 uint32_t check_equal(uint16_t* x, uint16_t* y);
+
+uint32_t check_gteq(uint16_t* x, uint16_t* y);
+
+uint32_t check_gt(uint16_t* x, uint16_t* y);
 
 #endif
